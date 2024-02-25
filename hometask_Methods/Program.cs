b71c1ec0641d04610f73ad4e54b3f9200d2a47b0
tebr1z <tabrizyh@code.edu.az>
2024-02-26 00:45:18 +0400

    Proje dosyası ekle.

---
+++ hometask_Methods/Program.cs
@@ -0,0 +1,133 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace hometask_Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region task 1 void
+            //Console.WriteLine("1 ci deyer daxil edin");
+            //int num1=int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("2 ci deyer daxil edin");
+            //int num2 =int.Parse(Console.ReadLine());
+
+            //if (num1 < num2)  Console.WriteLine("1 deyere 2 ci deyerden boyuk ola bilmez");
+
+            //else   Console.WriteLine("Cavab:   "+task1(num1, num2) );
+            #endregion
+
+
+            #region task 2void
+            //    int[] numbers = { 1, 2, 3, 4, 5 };
+
+            //int TT = CollectionArray(numbers);
+            //    Console.WriteLine(TT);
+            #endregion
+
+            #region Task 3 Void
+
+            //int n = 4; 
+            //CheckPrime(n);
+
+
+            #endregion
+
+            #region Task 4 void
+
+            //int n = 16; 
+            //CheckPowerOfTwo(n);
+            #endregion
+        }
+
+        #region task1`in int static
+        //static int task1(int a, int b)
+        //{
+        //    return a / b;
+        //}
+
+        #endregion
+
+        #region task2`in Statici
+        //static int CollectionArray (int[] numbers)
+        //{
+
+        //    int sum = 0;
+        //    for (int i = 0; i<numbers.Length; i++)
+        //    {
+        //        sum += numbers[i];
+
+        //    }
+
+
+
+
+        //    return sum;
+        //}
+
+
+
+        #endregion
+
+        #region Task 3
+        //public static void CheckPrime(int n)
+        //{
+        //    if (n <= 1) 
+        //    {
+        //        Console.WriteLine(n + "1 balaca reqem olmaz");
+        //        return;
+        //    }
+
+
+        //    for (int i = 2; i <= n / 2; i++)
+        //    {
+        //        if (n % i == 0) 
+        //        {
+        //            Console.WriteLine(n + " sayi mukrekeb deyil");
+        //            return;
+        //        }
+        //    }
+
+
+        //    Console.WriteLine(n + " Sayi murekebdi");
+        //}
+
+
+        #endregion
+
+
+        #region task 4 
+
+        //public static void CheckPowerOfTwo(int n)
+        //{
+        //    if (n <= 0) 
+        //    {
+        //        Console.WriteLine(n + "  Sayi 2 quvveti deyil");
+        //        return;
+        //    }
+
+
+        //    while (n % 2 == 0)
+        //    {
+        //        n /= 2;
+        //    }
+
+        //    if (n == 1)   Console.WriteLine("2 Quvvetidir.");
+
+        //    else   Console.WriteLine("2 Quvveti deyil");
+
+        //}
+
+
+        #endregion
+
+
+
+
+        #region Taks 5
+
+        #endregion
+
+    }
+}
